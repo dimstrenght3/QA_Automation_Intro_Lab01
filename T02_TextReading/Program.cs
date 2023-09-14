eff7c29cdb4905e0ea9df2f28bb60075195d4a71
Dimo Dimitrov <137059193+dimstrenght3@users.noreply.github.com>
2023-09-14 18:34:08 +0300

    Create Program.cs

---
+++ T02_TextReading/Program.cs
@@ -0,0 +1,11 @@
+namespace T02_TextReading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Console.WriteLine(text);
+        }
+    }
+}
