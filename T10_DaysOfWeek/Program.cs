a6c3afd9fe14469c2641351a45d7974470a3394a
Dimo Dimitrov <137059193+dimstrenght3@users.noreply.github.com>
2023-09-14 18:47:44 +0300

    Create Program.cs

---
+++ T10_DaysOfWeek/Program.cs
@@ -0,0 +1,39 @@
+namespace T10_DaysOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sonday");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break; 
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
