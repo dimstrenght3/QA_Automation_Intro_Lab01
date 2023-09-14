625b22c34e3aa155287bbf34d7032da55124c773
Dimo Dimitrov <137059193+dimstrenght3@users.noreply.github.com>
2023-09-14 18:38:05 +0300

    Create Program.cs

---
+++ T06_ExcelentGradeCheck/Program.cs
@@ -0,0 +1,20 @@
+namespace T06_ExcellentGradeCheck
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade <= 5.50) 
+            {
+                Console.WriteLine("Excellent!");
+            }
+            else 
+            {
+                Console.WriteLine("Not Excellent.");
+            }
+            
+        }
+    }
+}
