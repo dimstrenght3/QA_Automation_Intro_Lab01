bcd513ea1f669ef06943380d7d6e1c48bc6eba9c
Dimo Dimitrov <137059193+dimstrenght3@users.noreply.github.com>
2023-09-14 18:35:32 +0300

    Create Program.cs

---
+++ T03_SquareArea/Program.cs
@@ -0,0 +1,12 @@
+namespace T03_SquareArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int side = int.Parse(Console.ReadLine());
+            int area = side*side;
+            Console.WriteLine(area);
+        }
+    }
+}
